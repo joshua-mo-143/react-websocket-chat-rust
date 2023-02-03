484b630cbbd20681e8cec8dc08701318969abb22
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-02-03 12:06:04 +0000

    refactor: minor changes, add default join message

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,11 @@ function App() {
       <div className="flex flex-row text-gray-100">
         <div className='w-full bg-slate-700 flex flex-col pb-5' >
           <div className='w-full min-h-screen flex flex-col justify-end gap-4 pb-20' id="chatbox">
-              <div className="mx-8 break-all chat-message bg-slate-600 rounded-xl rounded-xl w-fit inline-block px-5 py-4">
-                <p>Hi! Welcome to Rustcord. Enjoy your stay!</p>
-              </div>
+            <div className="mx-8 break-all chat-message bg-slate-600 rounded-xl rounded-xl w-fit inline-block px-5 py-4">
+              <p>Hi! Welcome to Rustcord. Enjoy your stay!</p>
+            </div>
           </div>
-          <form className='w-full h-10 fixed bottom-0 flex flex-row mb-5 px-5' onSubmit={(e) => sendMessage(e,name, message, setMessage)}>
+          <form className='w-full h-10 fixed bottom-0 flex flex-row mb-5 px-5' onSubmit={(e) => sendMessage(e, name, message, setMessage)}>
             <input name="message" id="messageBox" type="text" className='bg-slate-400 w-full py-2 px-5 focus:outline-0 rounded-tl-xl rounded-bl-xl' value={message}
               placeholder="Enter your message here..."
               onInput={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} />
@@ -33,7 +33,8 @@ function App() {
             </button>
           </form>
         </div>
-      </div></>
+      </div>
+    </>
 
   )
 }
